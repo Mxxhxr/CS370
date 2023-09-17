0436097c4f4079bc32f80a849380b0ea571756a0
Maahir Vohra <117885945+Mxxhxr@users.noreply.github.com>
2023-09-17 11:48:38 -0400

    hw1

---
+++ MaahirVohra_Assignment1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import scipy as sp
+import numpy as np
+
+#Problem 1A
+gs = np.random.normal(size=1000, loc=7.25, scale=0.875)
+
+#Problem 1A
+
+ak = np.random.multivariate_normal()
